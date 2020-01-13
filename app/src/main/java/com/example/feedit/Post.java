0ad795772636a2941bb43510f1102b55a753197f
yn12hh <yonatanhof11@gmail.com>
2020-01-13 17:45:15 +0200

    Added the register and sign in screens, as well as the Post class and updated drawables, and styles and strings xmls are updated as well.

---
+++ app/src/main/java/com/example/feedit/Post.java
@@ -0,0 +1,44 @@
+package com.example.feedit;
+
+
+import android.widget.Button;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Post {
+    private String title;
+    private String team;
+    private Date time_stamp =  new Date();
+    private final Calendar c = Calendar.getInstance();
+    private String poster_name;
+    private String post_text;
+    private String project;
+
+    //getters
+    public String getTitle(){
+       return title;
+    }
+
+    public String getTeam(){
+        return team;
+    }
+
+    public String getPoster_name(){
+        return poster_name;
+    }
+
+    public String getPost_text(){
+        return post_text;
+    }
+
+    public String getProject(){
+        return project;
+    }
+
+    public Date getTime_stamp() { return time_stamp; }
+
+
+}
