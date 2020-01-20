3043feb4221317a4528aebbafc528700a77eb868
AMIT Z <AMITZOO123@GMAIL.COM>
2020-01-20 14:12:03 +0200

    adding getters and setters to LocalUserInfo
    adding getters and setters to both fields of the class
    also handeled a few static and constructor issues

--- app/src/main/java/com/example/feedit/LocalUserInfo.java
+++ app/src/main/java/com/example/feedit/LocalUserInfo.java
@@ -1,7 +1,7 @@
 package com.example.feedit;
 
 public class LocalUserInfo {
-    private LocalUserInfo  localUserInfo_instance = null;
+    private static LocalUserInfo  localUserInfo_instance = null;
     private String last_used_user_name;
     private String last_used_password;
 
@@ -9,9 +9,26 @@ public class LocalUserInfo {
         last_used_user_name = "";
         last_used_password = "";
     }
-    public LocalUserInfo getInstance() {
+    public static LocalUserInfo getInstance() {
         if(localUserInfo_instance == null)
             localUserInfo_instance = new LocalUserInfo();
         return localUserInfo_instance;
+    }
+
+
+    public String getLast_used_user_name() {
+        return last_used_user_name;
+    }
+
+    public void setLast_used_user_name(String last_used_user_name) {
+        this.last_used_user_name = last_used_user_name;
+    }
+
+    public String getLast_used_password() {
+        return last_used_password;
+    }
+
+    public void setLast_used_password(String last_used_password) {
+        this.last_used_password = last_used_password;
     }
 }
