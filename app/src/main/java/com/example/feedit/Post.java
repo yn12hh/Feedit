33ec2bc65d2695b452122fc722a07419c88f1142
AMIT Z <AMITZOO123@GMAIL.COM>
2020-01-23 12:22:19 +0200

    changing post and FUFBI to accomedate adapter

--- app/src/main/java/com/example/feedit/Post.java
+++ app/src/main/java/com/example/feedit/Post.java
@@ -14,22 +14,22 @@ import java.util.Locale;
 import java.util.Objects;
 
 public class Post {
-    private String title;
-    private String team;
-    private Date time_stamp;
+    private final String title;
+    private final String team;
+    private final Date time_stamp;
     private final Calendar c = Calendar.getInstance();
     private String author;
-    private String post_text;
-    private String project;
+    private final String post_text;
+    private final String project;
     private SimpleDateFormat timestamp_format = new SimpleDateFormat("HH:mm dd/mm/yyyy", Locale.getDefault());
 
     public Post(String title, String team, String post_text, String project) {
         this.title = title;
         this.team = team;
-        if((this.author = FirebaseAuth.getInstance().getCurrentUser().getEmail()) == null)
-            this.author = "Anonymous";
+        if((author = FirebaseAuth.getInstance().getCurrentUser().getEmail()) == null)
+            author = "Anonymous";
         else
-            this.author = this.author.substring(0, this.author.indexOf('@'));
+            author = author.substring(0, author.indexOf('@'));
         this.post_text = post_text;
         this.project = project;
         this.time_stamp = new Date();
@@ -58,29 +58,4 @@ public class Post {
 
     public Date getTime_stamp() { return time_stamp; }
 
-    public String getTime_stampAsString() { return timestamp_format.format(time_stamp);}
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public void setTime_stamp(Date time_stamp) {
-        this.time_stamp = time_stamp;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPost_text(String post_text) {
-        this.post_text = post_text;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
 }
