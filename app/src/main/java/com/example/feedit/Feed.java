3f0ceaaeac95a1ea68886d86dafe7ea9bfe63956
AMIT Z <AMITZOO123@GMAIL.COM>
2020-01-26 03:09:06 +0200

    the first working feed in the app!!!

--- app/src/main/java/com/example/feedit/Feed.java
+++ app/src/main/java/com/example/feedit/Feed.java
@@ -2,7 +2,10 @@ package com.example.feedit;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -14,30 +17,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.feedit.Post;
 import com.example.feedit.R;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
-public class Feed extends AppCompatActivity {
+public class Feed extends AppCompatActivity  {
 
     private ImageView add_button;
     private Dialog  new_post_dialog;
     private ImageView close_dialog;
     private Button post_button_clicked;
     private FeedItFBInterface FBI;
+    private EditText title_et, projects_et, teams_et, post_content_et;
+    private TextView title_tv,teams_tv, projects_tv, post_content_tv, author_tv; //time?
+
+    private FirebaseFirestore firestore_database = FirebaseFirestore.getInstance();
+    private CollectionReference feed_it_posts_ref = firestore_database.collection("entries");
+
+    private FeedAdapter feed_adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        setUpRecyclerView();
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FeedFragment()).commit();
+        //missing author_tv = (TextView) findViewById(), the part that creates the text
 
-        new_post_dialog = new Dialog(this);
 
         FBI = new FeedItFBInterface();
 
@@ -46,74 +60,89 @@ public class Feed extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
+
                 showAddPost();
             }
         });
 
+    }
 
+
+    private void setUpRecyclerView(){
+        Query query = feed_it_posts_ref.orderBy("timestamp", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Post> options = new FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post.class).build();
+        feed_adapter = new FeedAdapter(options);
+        RecyclerView feed_recycler = findViewById(R.id.feed_recycler_view);
+        //for performance reasons: to enhance the performance
+        feed_recycler.setHasFixedSize(true);
+        feed_recycler.setLayoutManager(new LinearLayoutManager(this));
+        feed_recycler.setAdapter(feed_adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        feed_adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        feed_adapter.stopListening();
+    }
+
+
+
+   public void showAddPost(){
+
+
+       final AlertDialog.Builder mbuilder = new AlertDialog.Builder(Feed.this);
+       View mview = getLayoutInflater().inflate(R.layout.add_post_dialog, null);
+
+       mbuilder.setView(mview);
+       new_post_dialog = mbuilder.create();
+       new_post_dialog.show();
+
+       title_et = (EditText) mview.findViewById(R.id.title_input);
+       teams_et = (EditText) mview.findViewById(R.id.teams_name_input);
+       projects_et = (EditText) mview.findViewById(R.id.projects_name_input);
+       post_content_et = (EditText) mview.findViewById(R.id.posts_content_input);
+       post_button_clicked = (Button) mview.findViewById(R.id.post_button);
+       post_button_clicked.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               String title = title_et.getText().toString();
+               String teams = teams_et.getText().toString();
+               String projects = projects_et.getText().toString();
+               String post_content = post_content_et.getText().toString();
+               if(!title.isEmpty() && !teams.isEmpty() && !projects.isEmpty() && !post_content.isEmpty()) {
+
+                   sendDetailsToFB(title, teams, projects, post_content);
+                   new_post_dialog.dismiss();
+               }
+                else {
+
+                   Toast.makeText(getApplicationContext(),"You left one of the fields empty, please try again", Toast.LENGTH_LONG).show();
+               }
+           }
+       });
+
+       close_dialog = (ImageView) mview.findViewById(R.id.close_dialog_imag);
+       close_dialog.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               new_post_dialog.dismiss();
+           }
+       });
 
 
     }
-
-    public void showAddPost(){
-        new_post_dialog.setContentView(R.layout.add_post_dialog);
-        close_dialog = (ImageView) new_post_dialog.findViewById(R.id.close_dialog_imag);
-        post_button_clicked = (Button) new_post_dialog.findViewById(R.id.post_button);
-
-        close_dialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new_post_dialog.dismiss();
-            }
-        });
-
-        new_post_dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        new_post_dialog.show();
-
-        post_button_clicked.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*EditText title = (EditText) findViewById(R.id.title_input);
-                String title_string =   title.getText().toString();
-                EditText teams_name = (EditText) findViewById(R.id.teams_name_input);
-                String teams_name_string =  teams_name.getText().toString();
-                //EditText user_ID = (EditText) findViewById(R.id.txtInput_userID);
-                String user_ID_string =  "1234";
-                EditText posts_content = (EditText) findViewById(R.id.posts_content_input);
-                String posts_content_string =  posts_content.getText().toString();
-                EditText projects_name = (EditText) findViewById(R.id.projects_name_input);
-                String projects_name_string =  projects_name.getText().toString();*/
-                //Post new_post_object = new Post(name_string ,teams_name_string, user_ID_string, posts_content_string, projects_name_string);
-                //pass to firebase!;
-
-                /*Post post = new Post("come see the new board!", "marketing", "naftali", "it's ready!!!", "luach_bogrim");
-                FBI.uploadPost(post);*/
-                new_post_dialog.dismiss();
-
-            }
-        });
+    public void sendDetailsToFB(String title, String teams, String projects, String post_content) {
+        Post new_post = new Post(title,teams,post_content, projects);
+        FBI.uploadPost(new_post);
+        new_post_dialog.dismiss();
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment selectedFragment = null;
-
-            switch (menuItem.getItemId()) {
-                case R.id.nav_feed:
-                    selectedFragment = new FeedFragment();
-                    break;
-                case R.id.nav_team:
-                    selectedFragment = new TeamsFragment();
-                    break;
-                case R.id.nav_projects:
-                    selectedFragment = new ProjectsFragment();
-                    break;
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-            return true;
-        }
-    };
 
 
 }
